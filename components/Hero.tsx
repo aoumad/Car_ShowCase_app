c67ed3c3e6595968508e5820b2546dfb68b1c8ef
aoumad <abderazzakoumad@gmail.com>
2024-02-18 11:03:41 +0100

    moving to a more helpful project

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,9 +31,9 @@ const Hero = () => {
                     <Image src="/hero.png" alt="hero"
                     fill className='object-contain'
                     />
-                    <div className='hero__image-overlay'>
+                </div>
+                    <div className='hero__image-overlay' >
                         
-                    </div>
                 </div>
             </div>
         </div>
